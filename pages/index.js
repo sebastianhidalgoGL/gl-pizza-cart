280c34c998d73823e969aec8c1bc1a1b5ab89270
nanuck <87029445+nanuck@users.noreply.github.com>
2021-12-04 19:35:19 -0600

    test
    test

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from './index.module.css'
 const Home = () => (
   <div className={styles.container}>
     <Head>
-      <title>Create Next App</title>
+      <title>PizzaCart</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
