65f97fa6209f5624dfe84b5123674454ee864f50
nanuck <95543489+sebastianhidalgoGL@users.noreply.github.com>
2021-12-23 00:06:53 -0600

    tailwind setup
    * Setting up Tailwind css for Nextjs
    * Upgrading packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/global.css'
+import 'tailwindcss/tailwind.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
