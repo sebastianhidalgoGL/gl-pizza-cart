d05a191841afa76ebcffdeb521d4d602b250bc88
nanuck <95543489+sebastianhidalgoGL@users.noreply.github.com>
2021-12-27 15:54:01 -0600

    Typescript set up

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from 'next/app'
+import React from 'react'
+import '../styles/tailwind.css'
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
